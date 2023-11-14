f288edf76a847fa0f22cd513b76c11e1e3c9bed7
Abdelrahman Essa <100742471+AbdelrahmanIssa@users.noreply.github.com>
2023-11-14 15:57:13 -0800

    Add files via upload

---
+++ Quiz/js/main.js
@@ -0,0 +1,85 @@
+
+import { Setting } from "./setting.js";
+new Setting()  // call constructor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***
+ java , scheme , self 
+ internal , external , inline
+var 
+data types :primitive string  , number , boolean , null , undefined , nonprimitive:object
+concatnication
+conditions
+loops : for , while , dowhile 
+function
+DOM: document object model
+  1-select element
+  2-add event
+  3-action
+  HTMLcollection :  length , [0] , loops 
+  NodeList :  length , [0] , loops 
+  setAtribute , getAtribute
+Api => App Programming interface (url)
+facebookapi.signUp
+facebookapi.signOut
+Json => web , mobile , desktop , IOS
+readystate => 0,1,2,3,4
+status => 404 , 403 , 200
+sync , async
+callback 
+promise
+await - async
+oop =>object orianted programming (technique simulate reality)
+1-class based (calss , objects)
+2-prototype based  =>  (object , objects) js   (suger syntax )
+this:
+1- من غير اي حاجه => window
+2- function => window
+3- method => object
+4- event => elemnt
+spreadOperator => ...
+let , const 
+`${   }`
+default params 
+NAN 
+arrow function
+"use strict"
+Jquery => write less do more
+hide , show , toggle , fadeIn , fadeOut , slideUp , slideDown , slideToggle
+chaining
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
